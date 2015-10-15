47d7c8cebf5e21f819355e9e55204b5fd38a8c78
mountainwill <willhulburd@gmail.com>
2015-10-15 14:03:05 -0400

    My second, disastrous commit

---
+++ src/main/java/com/example/tutorial1/pages/Index.java
@@ -0,0 +1,12 @@
+package com.example.tutorial1.pages;
+
+import java.util.Date;
+import org.apache.tapestry5.annotations.*;
+import org.apache.tapestry5.ioc.annotations.*;
+import org.apache.tapestry5.corelib.components.*;
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.alerts.AlertManager;
+
+public class Index
+{
+}
